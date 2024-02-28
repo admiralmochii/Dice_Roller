49842d18676463c74a717f091e0b33f0481786c8
raighh <116239519+raighh@users.noreply.github.com>
2024-02-28 20:55:59 +0800

    Updated the login page.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -75,27 +75,9 @@ fun FitnessTrackerApp(navController: NavController) {
 
     }
 }
-@Composable
-fun LoginPage() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Text(text = "Login Page", fontSize = 24.sp)
-    }
-}
-
-@Preview // test comment
-@Composable
-fun PreviewFitnessTrackerApp() {
-    FitnessTrackerApp(rememberNavController())
-}
-
-
 @Preview
 @Composable
-fun Preview() {
+fun LoginPage() {
     // Use MaterialTheme for consistent styling and easy theme switching
     MaterialTheme {
         // Surface is a composable provided by Material Design that gives you a background
@@ -139,18 +121,27 @@ fun Preview() {
 
                 // Login Button
                 Button(
-                    onClick = { /* Handle the login logic here */ },
-                    // Modifier to set the width of the button to take the max width available
-                    modifier = Modifier.fillMaxWidth()
+                    onClick = { /* Login logic and handling over here. Should call another function? */ },
+                    // Modifier to set the width of the button
+                    modifier = Modifier.width(200.dp)
                 ) {
                     Text("LOGIN")
                 }
 
                 // Text to navigate to the Sign Up screen or forgot password
-                TextButton(onClick = { /* Navigate to sign up or forgot password */ }) {
+                TextButton(onClick = { /* Forgot password and signup logic here. Should call another function */ }) {
                     Text("Don't have an account? Sign up", color = MaterialTheme.colorScheme.primary) // Set the text color to the primary color of the theme
                 }
             }
         }
     }
 }
+
+@Preview // test comment
+@Composable
+fun PreviewFitnessTrackerApp() {
+    FitnessTrackerApp(rememberNavController())
+}
+
+
+
